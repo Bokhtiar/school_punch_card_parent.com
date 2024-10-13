1ddd6b9df57eede94b4452706386974de43152d9
bokhtiar <bokhtiartoshar1@gmail.com>
2024-10-13 21:08:02 +0600

    dashboard templete layout

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; <strong><span>It Media</span></strong>. All Rights Reserved
+    </div>
+</footer>
---
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -0,0 +1,39 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-grid"></i>
+                <span>Billing List</span>
+            </a>
+        </li><!-- End billing Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-grid"></i>
+                <span>Billing Create</span>
+            </a>
+        </li><!-- End billing Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-grid"></i>
+                <span>Role</span>
+            </a>
+        </li><!-- End role Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-grid"></i>
+                <span>Logout</span>
+            </a>
+        </li><!-- End billing Nav -->
+    </ul>
+</aside>
